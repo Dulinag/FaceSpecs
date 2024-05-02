c0d416e7d11da6bfe4e552c8c820ac9c8fe6ceb0
anthonypoon12 <anthony@poons.us>
2024-05-02 04:04:50 -0400

    Clean up comments

--- script.js
+++ script.js
@@ -121,23 +121,6 @@ video.addEventListener('play', async () => {
 
 });
 
-// This is a really bad way of loading the labeled images
-// function loadLabeledImages() {
-//   const labels = ['Anthony'];
-//   return Promise.all(
-//     labels.map(async label => {
-//       const descriptions = []
-//       for (let i = 1; i <= 2; i++) {
-//         const img = await faceapi.fetchImage(`/labeled_images/${label}/${i}.jpg`);
-//         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-//         descriptions.push(detections.descriptor);
-//       }
-
-//       return new faceapi.LabeledFaceDescriptors(label, descriptions);
-//     })
-//   )
-// }
-
 async function addEntry() {
   const imageInput = document.getElementById('imageInput');
   const label = document.getElementById('textInput').value;
